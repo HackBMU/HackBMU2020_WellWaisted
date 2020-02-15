4062bc1faeb2f6d8ec7725e0953d3634ccadefa4
vish198910 <vish198910@gmail.com>
2020-02-15 17:53:45 +0530

    Initiated Well Wasted

---
+++ lib/Pages/UserSignUpPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserSignUp extends StatefulWidget {
+  @override
+  _UserSignUpState createState() => _UserSignUpState();
+}
+
+class _UserSignUpState extends State<UserSignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
